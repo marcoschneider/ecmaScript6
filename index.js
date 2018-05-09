c5645431da21e54a9424c2db257859b59b800e62
Marco Schneider <marco.schneider@somedia.ch>
2018-05-09 09:46:57 +0200

    Added server and implemented purecss

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 'use strict';
 
+var express = require('express');
+var app = express();
+
+app.use('/purecss', express.static(__dirname + '/node_modules/purecss/build'));
+
+app.get('/', function (req, res) {
+  res.sendfile(__dirname + '/index.html');
+});
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
